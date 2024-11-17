084bfd9967b15c3fd91fae7829aaf3859eee7f5b
Ravin Yadav <yravin72@gmail.com>
2024-11-17 17:45:42 +0530

    make responsive Navbar added current temperature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,127 @@
+"use client"
+import Container from "@/components/Container";
+import Navbar from "@/components/Navbar";
+import { convertKelvintoCelsius } from "@/utils/convertKelvintoCelsius";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { format, parseISO } from "date-fns";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
+export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      );
+      return data;
+    },
+  });
+
+  const firstData=data?.list[0]
+  console.log("data", data?.city.name);
+  
+  if (isLoading) return(
+    <div className="flex items-center min-h-screen justify-center">
+      <p className="animate-bounce">Loading...</p>
+    </div>
+  );
+  
+  
+  return (
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
+      <Navbar/>
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* today data */}
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(firstData?.dt_txt??''),'EEEE')}</p>
+              <p className="text-lg">({format(parseISO(firstData?.dt_txt??''),'dd.MM.yyyy')})</p>
+            </h2>
+            <Container className="gap-10 px-6 items-center">
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                {convertKelvintoCelsius(firstData?.main.temp ?? 296.37)}°
+                </span> 
+                <p className="text-xs font-normal space-x-1 whitespace-nowrap">
+                  <span>Feels Like </span>
+                  <span>{convertKelvintoCelsius(firstData?.main.feels_like??0)}°</span>
+                </p>
+                <p className="text-xs font-semibold space-x-2">
+                      <span>
+                        {convertKelvintoCelsius(firstData?.main.temp_min ?? 0)}
+                        °↓{" "}
+                      </span>
+                      <span>
+                        {" "}
+                        {convertKelvintoCelsius(firstData?.main.temp_max ?? 0)}
+                        °↑
+                      </span>
+                    </p>    
+              </div>
+                {/* Time and Weather icon */}
+            </Container>
+          </div>
+        </section>
+        {/* 7 days forecast data */}
+        <section></section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+/** @format */
+import * as React from 'react';
+import { MdWbSunny } from "react-icons/md";
+import { MdMyLocation } from "react-icons/md";
+import { MdOutlineLocationOn } from "react-icons/md";
+import SearchBox from './SearchBox';
+
+type Props = {}
+
+export default function Navbar({ }: Props) {
+    return (
+        <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white' >
+            <div className='h-[80px] w-full flex  justify-between items-center max-w-7xl px-3 mx-auto'>
+                <p className='flex items-center  justify-center gap-2'>
+                    <h2 className='text-gray-500 text-3xl'>Weather</h2>
+                    <span className="text-3xl mt-1 text-yellow-300">
+                        <MdWbSunny />
+                    </span>
+                </p>
+                {/* */}
+                <section className='flex gap-2 items-center'>
+                    <span className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer'>
+                    <MdMyLocation />
+                    </span>
+                    <span className='text-3xl'>
+                    <MdOutlineLocationOn />
+                    </span>
+                    <p className='text-slate-900/80 text-sm'>Gumsuda</p>
+                    {/* Search box */}
+                    <div>
+                        <SearchBox/>
+                    </div>
+                </section>
+            </div>
+        </nav>
+    )
+}
